6fe678e99858f6bb5e9418e6d068978917eb885f
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-01-04 17:34:11 +0100

    Add decrement funtion and buttons

--- script.js
+++ script.js
@@ -11,9 +11,17 @@ var Counter = React.createClass({
         });
     },
 
-    render: function() {
-        return React.createElement('div', {onClick: this.increment},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter -1
+        });
+    },
+
+    render: function(){
+        return React.createElement('div', {},
+            React.createElement('span', {}, ' Licznik ' + this.state.counter + ' '),
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.decrement}, '-')
         );
     }
 });
